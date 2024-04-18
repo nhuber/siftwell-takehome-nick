b2f8253484771e3b2f723b6489871a5e6164c947
nhuber <nick@thinkingmachin.es>
2024-04-18 09:02:22 +0000

    more data

--- Hello.py
+++ Hello.py
@@ -23,8 +23,15 @@ from streamlit.hello.utils import show_code
 import pickle
 import lzma
 
+import sklearn
+
 def model_explorer():
-    regr_best_new = pickle.load(lzma.open('regr_best.xz'))
+    regr_best = pickle.load(lzma.open('regr_best.xz'))
+    rf_cf_best = pickle.load(lzma.open('rf_cf_best.xz'))
+    df_train_prepared = pickle.load(lzma.open('df_train_prepared.xz'))
+    full_pipeline = pickle.load(lzma.open('full_pipeline.xz'))
+
+    st.slider('Slide me', min_value=0, max_value=10)
 
     st.set_page_config(page_title="Siftwell Take-Home Model Explorer", page_icon="📊")
     st.markdown("# Siftwell Take-Home Model Explorer")
